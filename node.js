57d14832e2da7cff7f8923542e3c5374abbce024
ApxuTechTop <ApxuTechTop@users.noreply.github.com>
2025-06-19 15:33:12 +0000

    Deployed using Blazing fast GitHub Pages deploy

--- node.js
+++ node.js
@@ -14414,9 +14414,7 @@ var $;
             Blocks: $hyoo_crus_list_ref_to(() => $apxutechtop_samosbor_map_block_data),
         }) {
             static global() {
-                const storage = $hyoo_crus_glob.home(Gigacluster);
-                const ref = storage.ref().description;
-                console.log("global ref:", ref);
+                const storage = $hyoo_crus_glob.Node($hyoo_crus_ref("rIuXD13u_MIkær61B"), Gigacluster);
                 return storage;
             }
             static create_block() {
