89c5d4921c40cc4788aa26fe3e827df27a98cdc1
ApxuTechTop <apxutech@gmail.com>
2025-07-16 00:24:23 +0700

    Change my public key for prod

--- app/app.test.ts
+++ app/app.test.ts
@@ -1,4 +1,5 @@
 namespace $ {
 	$.$apxu_samosbor_map_app_gigacluster_ref = "rIuXD13u_MIkær61B"
 	$.$apxu_samosbor_map_app_roles_ref = "rIuXD13u_MIkær61B"
+	$.$apxu_samosbor_map_app_my_public_key = "_2Ksi5cPXe4wiT6vrUFLg0iJ1_x9y6zpY5bO4I1bNIINz7cHgATPJwQ08aZLFDc-e6pr8YPX8bymALGR-IbaW8"
 }
