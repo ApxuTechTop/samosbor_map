479987f436dc07ade5dd429cdba6f3fcd358ccbc
ApxuTechTop <apxutech@gmail.com>
2025-07-18 19:04:03 +0700

    Update refs

--- app/app.ts
+++ app/app.ts
@@ -1,6 +1,6 @@
 namespace $ {
-	export let $apxu_samosbor_map_app_storage_ref = ""
+	export let $apxu_samosbor_map_app_storage_ref = "QmLw5lJT_3uA1hzjn"
 	// export let $apxu_samosbor_map_app_gigacluster_ref = "MTrHG0xQ_1vO5NBzk"
 	// export let $apxu_samosbor_map_app_roles_ref = "MTrHG0xQ_1vO5NBzk"
-	export let $apxu_samosbor_map_app_my_public_key = "_3QxOscbTFDW87k0HOTD-LjjF-n0RFl-0XPcr6-pYbw4s_DXPzwqRJMkE9gzW8JrgmBmk5M6kNpIy0WZ1ZxxsE"
+	export let $apxu_samosbor_map_app_my_public_key = "_-lCYvDmUlPe4DWHOOeJ7BFeIEJuI-lHCJept_u19SAUWGIg-uRgUgcxEHw-wSCL6deScK98QcwnFBEKVKpD-Q"
 }
