dc3ba300d58d68e22fa244f9779a8e86891a27c4
Lyumih <m.ivanov1225@gmail.com>
2025-07-31 09:15:15 +0300

    Добавлено описание и ссылки

--- app/app.test.ts
+++ app/app.test.ts
@@ -1,3 +1,9 @@
+/**
+ * Аналог config.yaml или .env - окружение для локальной разработки.
+ * Нужно получить из консоли разработчика
+ * - ref на $hyoo_crus_auth.current().lord().description
+ * - public_key на свой $hyoo_crus_auth.current().public().toString()
+ */
 namespace $ {
 	$.$apxu_samosbor_map_app_storage_ref = "bGX3TLtt_JLD0OnSÆ"
 	// $.$apxu_samosbor_map_app_gigacluster_ref = "rIuXD13u_MIkær61B"
