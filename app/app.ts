e457e3b23a6148b56fba10faedd8f10d2371747b
ApxuTechTop <apxutech@gmail.com>
2025-08-05 23:26:26 +0700

    Remove comments

--- app/app.ts
+++ app/app.ts
@@ -1,6 +1,4 @@
 namespace $ {
 	export let $apxu_samosbor_map_app_storage_ref = "QmLw5lJT_3uA1hzjn"
-	// export let $apxu_samosbor_map_app_gigacluster_ref = "MTrHG0xQ_1vO5NBzk"
-	// export let $apxu_samosbor_map_app_roles_ref = "MTrHG0xQ_1vO5NBzk"
 	export let $apxu_samosbor_map_app_my_public_key = "_-lCYvDmUlPe4DWHOOeJ7BFeIEJuI-lHCJept_u19SAUWGIg-uRgUgcxEHw-wSCL6deScK98QcwnFBEKVKpD-Q"
 }
