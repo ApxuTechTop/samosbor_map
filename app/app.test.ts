6fceec0ba561977fc5f0505cc8b9a03ba14919f8
ApxuTechTop <apxutech@gmail.com>
2025-12-20 20:21:06 +0700

    Fix build error

--- app/app.test.ts
+++ app/app.test.ts
@@ -5,8 +5,8 @@
  * - public_key на свой $hyoo_crus_auth.current().public().toString()
  */
 namespace $ {
-	$.$apxu_samosbor_map_app_storage_ref = $hyoo_crus_auth.current().lord().description!
+	// $.$apxu_samosbor_map_app_storage_ref = $mol_wire_sync(() => $hyoo_crus_auth.current().lord().description!)()
 	// $.$apxu_samosbor_map_app_gigacluster_ref = "rIuXD13u_MIkær61B"
 	// $.$apxu_samosbor_map_app_roles_ref = "rIuXD13u_MIkær61B"
-	$.$apxu_samosbor_map_app_my_public_key = $hyoo_crus_auth.current().public().toString()
+	// $.$apxu_samosbor_map_app_my_public_key = $mol_wire_sync(() => $hyoo_crus_auth.current().public().toString())()
 }
